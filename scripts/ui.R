aaeeeb512627afe16455674045320b8f2c9f3c9f
Daniel Padfield <d.padfieldscfc@gmail.com>
2017-09-21 23:17:48 +0100

    Adding a shiny dashboard

--- scripts/ui.R
+++ scripts/ui.R
@@ -1,10 +1,28 @@
 # shiny app with leaflet to add graphs to the side
 # ui
 library(leaflet)
+library(shinydashboard)
 
-ui <- fluidPage(
-  br(),
-  column(8,leafletOutput("map", height="600px")),
-  column(4,br(),br(),br(),br(),plotOutput("plot", height="300px")),
-  br()
+header <- dashboardHeader(
+  title = "Parkruns UK"
 )
+
+body <- dashboardBody(
+  fluidRow(
+    column(width = 7,
+           box(width = NULL, solidHeader = TRUE,
+               leafletOutput("map", height = 500)
+           )),
+    column(width = 5,
+           box(width = NULL, plotOutput("plot", height = 220))
+    )
+  )
+)
+
+dashboardPage(
+  header,
+  dashboardSidebar(disable = TRUE),
+  body
+)
+    
+           
